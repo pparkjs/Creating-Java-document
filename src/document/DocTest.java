46067d2eddd6463647278862ea78cf5b60bb7d0b
pparkjs <qkrwjdtn741@naver.com>
2023-03-17 11:00:53 +0900

    내가 제작한 document를 테스트

--- src/document/DocTest.java
+++ src/document/DocTest.java
@@ -4,6 +4,7 @@ public interface DocTest {
 	public static void main(String[] args) {
 		JavaDocTest test = null;
 		
+		// 해당 메소드에 마우스 올리면 내가 제작한 문서 보임
 		test.methodAdd(0,0);
 		test.methodTest(0, 0);
 	}
